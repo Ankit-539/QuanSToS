fcba5c36966b76f347cce5b47362790b0b3567ef
Iqra210 <i.rasheed.1@research.gla.ac.uk>
2026-08-17 11:27:05 +0200

    product state
    Implemented random state generation using quantum states.

--- src/state_generator.py
+++ src/state_generator.py
@@ -15,6 +15,17 @@ def get_random_product(n: int, xp: ArrayNamespace, rng) -> Array:
     rho : (``2**n``, ``2**n``) Array
         Density matrix of a random state.
     """
+    psi_product = xp.asarray([1.0 + 0j])
+    for _ in range(n):
+        u = rng.uniform(-1.0, 1.0)
+        phi = rng.uniform(0.0, 2.0 * np.pi)
+        u = xp.asarray(u)
+        phi = xp.asarray(phi)
+        theta = xp.arccos(u)
+        psi = xp.stack([xp.cos(theta / 2), xp.exp(1j * phi) * xp.sin(theta / 2)])
+        psi_product = xp.reshape(psi_product[:, None] * psi[None, :], (-1,))
+    rho = xp.outer(psi_product, xp.conj(psi_product))
+    return rho
     pass
 
 
