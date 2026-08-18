8a8aeb4e07341fe9efb7d5cfeb67f4d62564dcd4
Ankit-539 <ankitjha539@gmail.com>
2026-08-18 11:16:05 +0200

    finished pauli measurement code - testing to be done

--- src/virtual_measurement_simulator.py
+++ src/virtual_measurement_simulator.py
@@ -1,5 +1,8 @@
 from array_api.latest import Array
 import array_api_compat
+from itertools import product
+import math
+import numpy as np
 
 
 def pauli_measurement(rho: Array, N: int) -> Array:
@@ -14,7 +17,7 @@ def pauli_measurement(rho: Array, N: int) -> Array:
         Density matrix of the measured state.
     N: int
         Number of times each measurement is repeated.
-        
+
 
     Returns
     -------
@@ -24,7 +27,7 @@ def pauli_measurement(rho: Array, N: int) -> Array:
         For example, for ``n=2`` the order will be:
 
             0: XX, 1: XY, 2: XZ, 3: YX, 4: YY, 5: YZ, 6: ZX, 7: ZY, 8: ZZ.
-         
+
         Columns represent measurement outcomes ``∈ {+, -}^n`` in the
         "lexicographic" order. For example, for ``n=2`` and every
         ``i∈[0, ..., M-1]`` one has:
@@ -35,8 +38,57 @@ def pauli_measurement(rho: Array, N: int) -> Array:
         with the i-th measurement setting.
     """
     xp = array_api_compat.array_namespace(rho)
-    counts = Array() # dummy
 
+    #finding number of qubits from density matrix
+    dim = rho.shape[0]
+    n = int(math.log2(dim))
+
+    #Defining the Pauli Matrices
+    I = xp.asarray([[1, 0], [0, 1]], dtype=rho.dtype, device=rho.device)
+    X = xp.asarray([[0, 1], [1, 0]], dtype=rho.dtype, device=rho.device)
+    Y = xp.asarray([[0, -1j], [1j, 0]], dtype=rho.dtype, device=rho.device)
+    Z = xp.asarray([[1, 0], [0, -1]], dtype=rho.dtype, device=rho.device)
+
+    #getting an array of corresponding projectors in order (X+, X-, Y+, Y-, Z+, Z-)
+    pauli_stacked = xp.stack([X, Y, Z])
+    projectors = xp.stack([(I+pauli_stacked)/2, (I-pauli_stacked)/2], axis=1)
+
+    #Defining Count Size
+    count_size = (3**n, 2**n)
+
+    #Defining the Pauli strings
+    pauli_strings = list(product(range(3), repeat=n))
+    output_strings = list(product(range(2), repeat=n))
+
+    count_rows = []
+    #Loop that iterates over each string to calculate probabilities observed
+    for setting_id, pauli_string in enumerate(pauli_strings):
+
+        probabilities = []
+        #Loop that goes overall all possible outcomes for a given input
+        for outcome_id, output_string in enumerate(output_strings):
+
+            #stores the projector corresponding to the pauli string
+            projector = projectors[pauli_string[0], output_string[0]]
+            for pauli_idx, outcome_idx in zip(pauli_string[1:], output_string[1:]):
+                projector = xp.kron(projector, projectors[pauli_idx, outcome_idx])
+
+            prob = xp.trace(xp.matmul(rho, projector))
+            probabilities.append(xp.real(prob))
+
+        probabilities = xp.stack(probabilities)
+
+        # Normalize probabilities
+        probabilities = probabilities / xp.sum(probabilities)
+
+        # Multinomial sampling
+        numpy_probabilities = array_api_compat.to_numpy(probabilities)
+        sampled_counts_np = np.random.multinomial(N, numpy_probabilities.tolist())
+        sampled_counts_xp = array_api_compat.asarray(sampled_counts_np, dtype=xp.int64, device=rho.device)
+
+        count_rows.append(sampled_counts_xp)
+
+    counts = xp.stack(count_rows)
 
     assert xp.all(xp.sum(counts, axis=-1) == N) # Sanity check
     return counts
